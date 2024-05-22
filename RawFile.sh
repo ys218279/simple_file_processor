f3aedc0762bb098fa9d4d4f00179fcb08a056434
Youness <129855046+ys218279@users.noreply.github.com>
2024-05-22 10:55:27 +0100

    file_processor.sh

--- RawFile.sh
+++ RawFile.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-INFILE="$1"
+#Create assets dir
+CURRENT_DIR=$(pwd)
+echo -e $CURRENT_DIR '\n'
+mkdir -p "${CURRENT_DIR}/assets"
 
-while IFS= read -r LINE
-do
-    echo "$LINE"
-done < "$INFILE"
+#Parse JSON file
+JSON_FILE="dependencies.json"
+CONTENT=$(jq -r '.dependencies[] | to_entries[]' -c "$JSON_FILE")
+
+#Create sub dir and download files
+for i in $CONTENT; do
+    dependency=$(echo $i | jq -r '.key')
+    url=$(echo $i | jq -r '.value')
+    echo -e "$dependency : $url" '\n'
+    #Create sub_dir
+    file_path="${CURRENT_DIR}/assets/${dependency}"
+    echo $file_path
+    mkdir -p "${file_path}"
+    #downlaod files in folder
+    wget "$url" -O "${file_path}/$(basename "$url")"
+done
+
